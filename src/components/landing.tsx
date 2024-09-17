f822c66d5dea37d2e1edcd215b22971fa3310a7b
rolmovel <r.olmo.ext@orangebank.es>
2024-09-17 09:01:06 +0200

    Cliente mailgun

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -20,23 +20,27 @@ const Landing: React.FC<LandingProps> = ({ posts }) => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [responseMessage, setResponseMessage] = useState('');
+  const formData = require('form-data');
+  const Mailgun = require('mailgun.js');
+  const mailgun = new Mailgun(formData);
+  const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || '7a3af442-4d5256b2'});
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      sendEmail('rolmovel@gmail.com', 'Asunto del correo', message);
+      mg.messages.create('sandbox-123.mailgun.org', {
+          from: "rolmovel@gmail.com",
+          to: ["rolmovel@gmail.com"],
+          subject: "Hello",
+          text: "Testing some Mailgun awesomness!",
+          html: "<h1>Testing some Mailgun awesomness!</h1>"
+        })
+        .then(msg => console.log(msg)) // logs response data
+        .catch(err => console.error(err)); // logs any error
     } catch (error) {
       setResponseMessage('Error al enviar el correo. Inténtalo de nuevo.');
     }
-  };
-
-async function sendEmail(to: string, subject: string, text: string): Promise<void> {
-  const data = {
-    from: 'TuNombre <tu-email@tudominio.com>',
-    to: to,
-    subject: subject,
-    text: text,
   };
 
   try {
